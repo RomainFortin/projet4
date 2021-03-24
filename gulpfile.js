a81d5cd7f7c301c9397829d949995a7247fde3c7
Romain Fortin <fortin.romain@gmail.com>
2021-03-24 21:05:09 +0100

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,30 @@
 const gulp = require('gulp');
+const rename =require("gulp-rename");
+const { series } = require('gulp');
 const purgecss = require('gulp-purgecss');
 const cleanCSS = require('gulp-clean-css');
 
 
 function style () {
-        return gulp.src('css_source/**/*.css')
+        return gulp.src(['css_source/**/*.css', '!css_source/css/bootstrap.css'])
             .pipe(purgecss({
                 content: ['**/*.html']
             }))
             .pipe(cleanCSS({compatibility: 'ie8'}))
+            .pipe(rename({ suffix: '.min' }))
             .pipe(gulp.dest('./'))
 }
 
+function bootstrap () {
+        return gulp.src(['css_source/css/bootstrap.css'])
+            .pipe(cleanCSS({compatibility: 'ie8'}))
+            .pipe(rename({ suffix: '.min' }))
+            .pipe(gulp.dest('./css'))
+}
+
 function watch() {
-    gulp.watch('css_source/**/*.css', style);
+    gulp.watch('css_source/**/*.css', series(style, bootstrap));
 };
 
 
-
-exports.style = style;
 exports.watch = watch;
