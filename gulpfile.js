e1ab6bf0310959990bc2044f9132294f43760ed5
Romain Fortin <fortin.romain@gmail.com>
2021-04-01 14:13:32 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,38 @@
 const gulp = require('gulp');
-const rename =require("gulp-rename");
-const { series } = require('gulp');
+const rename = require("gulp-rename");
+const {
+    series
+} = require('gulp');
 const purgecss = require('gulp-purgecss');
 const cleanCSS = require('gulp-clean-css');
 
-
-function style () {
-        return gulp.src(['css_source/**/*.css', '!css_source/css/bootstrap.css'])
-            .pipe(purgecss({
-                content: ['**/*.html']
-            }))
-            .pipe(cleanCSS({compatibility: 'ie8'}))
-            .pipe(rename({ suffix: '.min' }))
-            .pipe(gulp.dest('./'))
+function style() {
+    return gulp.src(['css_source/**/*.css', '!css_source/css/bootstrap.css'])
+        .pipe(purgecss({
+            content: ['**/*.html']
+        }))
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./'))
 }
 
-function bootstrap () {
-        return gulp.src(['css_source/css/bootstrap.css'])
-            .pipe(cleanCSS({compatibility: 'ie8'}))
-            .pipe(rename({ suffix: '.min' }))
-            .pipe(gulp.dest('./css'))
+function bootstrap() {
+    return gulp.src(['css_source/css/bootstrap.css'])
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./css'))
 }
 
 function watch() {
     gulp.watch('css_source/**/*.css', series(style, bootstrap));
 };
-
 
 exports.watch = watch;
